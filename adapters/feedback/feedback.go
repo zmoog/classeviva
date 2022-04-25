63a7d9a2ba8f6d8c4206c126a19ddcfbd3a787d2
Maurizio Branca <maurizio.branca@gmail.com>
2022-04-25 08:34:37 +0200

    Add tests for feedback

--- adapters/feedback/feedback.go
+++ adapters/feedback/feedback.go
@@ -23,13 +23,6 @@ func (fb *Feedback) Println(v interface{}) {
 	fmt.Fprintln(fb.out, v)
 }
 
-// type Feedback interface {
-// 	Println(a ...interface{}) (n int, err error)
-// }
-
-// type Console struct {
-// }
-
-// func (c Console) Println(a ...interface{}) (n int, err error) {
-// 	return fmt.Println(a...)
-// }
+func (fb *Feedback) Error(v interface{}) {
+	fmt.Fprintln(fb.err, v)
+}
